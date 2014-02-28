1c743d612b7548668b8d23a6c0f3b4e7e2c19e25
Michael Dupuis Jr <michael.dupuis@dockyard.com>
2014-02-28 09:22:39 -0500

    Adds `capybara-extentions` to the Gemfile.
    Requires Capybara and CapybaraExtensions in the `test_helper.rb`.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,9 @@ ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+require 'capybara/rails'
+require 'capybara-extensions'
+
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
 
